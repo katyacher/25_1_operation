3c10aa79faa2b17d35e77248340aa02745606cde
kate <katyacher@inbox.ru>
2022-12-08 11:20:27 +0300

    build moved to .gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kate/skillbox/code/25.1_operation
-# Build directory: /home/kate/skillbox/code/25.1_operation/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kate/skillbox/code/25.1_operation/src
-# Build directory: /home/kate/skillbox/code/25.1_operation/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
